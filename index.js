83ef1fd9fc6a4d6bc5c36787cb5083a20ffe199a
Afunnilo <57325456+Afunnilo@users.noreply.github.com>
2021-05-18 02:39:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const MongoClient = require('mongodb').MongoClient
 
 // set up mongoose
 const mongoose = require('mongoose');
-const connectionString = process.env.connection_string
+const connectionString = process.env.MONGO_URI
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
